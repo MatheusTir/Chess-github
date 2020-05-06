de47e376b23930fac3d536f10a293ea2c2aa2ed1
MatheusTir <tirabassi.matheus@aluno.ifsp.edu.br>
2020-05-06 16:07:20 -0300

    Class rook finnaly running

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -1,7 +1,8 @@
 package chess;
 
-import boardGame.Board;
-import boardGame.Piece;
+import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 
 public abstract class ChessPiece extends Piece {
 
@@ -16,4 +17,8 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 
+	protected boolean isThereOpponentPiece(Position position) {
+		ChessPiece p = (ChessPiece) getBoard().piece(position);
+		return p != null && p.getColor() != color;
+	}
 }
